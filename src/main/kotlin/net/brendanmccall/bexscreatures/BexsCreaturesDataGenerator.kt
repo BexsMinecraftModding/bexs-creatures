c60cdc71ef8a41addf99ce9c85d90d204829d0cd
Brendan McCall <brendanmccall3@gmail.com>
2026-04-21 20:28:12 -0600

    Initial commit

---
+++ src/main/kotlin/net/brendanmccall/bexscreatures/BexsCreaturesDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.brendanmccall.bexscreatures
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BexsCreaturesDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
